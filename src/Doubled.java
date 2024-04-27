d3900d196ec58ee1e46580a1ea3b64720ee8cbd0
Pia <pia.koester@hey.com>
2024-04-28 00:10:44 +0200

    reformat: refactor to follow review

--- src/Doubled.java
+++ src/Doubled.java
@@ -7,8 +7,8 @@ public class Doubled {
     public static int[] map(int[] arr) {
         int[] newArray = new int[arr.length];
 
-        for (int i = 0; i < arr.length; i++){
-            newArray[i] = arr[i] *2;
+        for (int i = 0; i < arr.length; i++) {
+            newArray[i] = arr[i] * 2;
         }
         return newArray;
     }
--- src/Space.java
+++ src/Space.java
@@ -2,12 +2,14 @@ public class Space {
     //https://www.codewars.com/kata/57eae20f5500ad98e50002c5/java
     //Write a function that removes the spaces from the string, then return the resultant string.
 
-    public static String noSpace( String x) {
-        String noSpace ="";
+    public static String noSpace(String x) {
+        String noSpace = "";
         char[] ch = x.toCharArray();
 
-        for (int i = 0; i< ch.length; i++){
-            if ( ch[i] != ' ') { noSpace = noSpace.concat(String.valueOf(ch[i]));}
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] != ' ') {
+                noSpace = noSpace.concat(String.valueOf(ch[i]));
+            }
 
         }
 
