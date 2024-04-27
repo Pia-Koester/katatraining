77559d9e5fb27b70e78f4cab5c995db6c6eb24fd
Pia <pia.koester@hey.com>
2024-04-28 00:08:01 +0200

    kata 5 trafficlight

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import java.util.Arrays;
 
 
-
 public class Main {
     public static void main(String[] args) {
 
         //To test Kata 1 - find value in array
-        boolean result =  ArrayCheck.check(new Object[] {66, 101, 88}, 88);
-        System.out.println("kata 1: " +result);
+        boolean result = ArrayCheck.check(new Object[]{66, 101, 88}, 88);
+        System.out.println("kata 1: " + result);
 
         String beast = "Papagei";
         String dish = "Porridge";
@@ -17,20 +16,17 @@ public class Main {
         System.out.println("kata 2: " + result1);
 
         //To test Kata 3 - double array
-        int[] resultArray = Doubled.map(new int[] {9, 7, 3});
-        System.out.println("Kata 3: "+ Arrays.toString(resultArray));
+        int[] resultArray = Doubled.map(new int[]{9, 7, 3});
+        System.out.println("Kata 3: " + Arrays.toString(resultArray));
 
         //To test Kata 4 - removing spaces
         String result4 = Space.noSpace("8 j 8   mBliB8g  imjB8B8  jl  B");
         System.out.println("Kata 4" + result4);
+
+        //to test Kata 5 - traffic lights
+        String result5 = TrafficLights.updateLight("green");
+        System.out.println("Kata 5 "+ result5);
     }
-
-
-
-
-
-
-
 
 
 }
